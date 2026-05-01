c44fcfb26b3936d9ec831acd3ee74297561e6d38
edwi-11 <calero06edwin11@gmail.com>
2026-05-01 12:57:39 -0600

    correcciones de rutas

---
+++ js/SideBar.js
@@ -0,0 +1,10 @@
+/*function loadSideBar() {
+    fetch('SideBar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sideBarContainer').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el SideBar:', error));
+}
+
+loadSideBar();*/
