fb5192dfa397219bbc10cd105dc931badb17a68a
Diego Calero <diegocalero772@gmail.com>
2026-06-25 15:44:56 -0600

    Corrigiendo cargar clientes y CRUD completo

--- js/shared/services/cliente.service.js
+++ js/shared/services/cliente.service.js
@@ -4,29 +4,29 @@ import { ClienteResponse } from '../models/response/cliente.response.js';
 export class ClienteService extends HttpService {
 
     async getClientes() {
-        const data = await this.get('/api/Cliente');
+        const data = await this.get('/Cliente');
         return data.map(item => ClienteResponse.fromJson(item));
     }
 
     async getClienteById(id) {
-        const data = await this.get(`/api/Cliente/${id}`);
+        const data = await this.get(`/Cliente/${id}`);
         return ClienteResponse.fromJson(data);
     }
 
     async getByTelefono(telefono) {
-        const data = await this.get(`/api/Cliente/telefono/${telefono}`);
+        const data = await this.get(`/Cliente/telefono/${telefono}`);
         return ClienteResponse.fromJson(data);
     }
 
     async createCliente(request) {
-        return await this.post('/api/Cliente', request.toJson());
+        return await this.post('/Cliente', request.toJson());
     }
 
     async updateCliente(id, request) {
-        return await this.put(`/api/Cliente/${id}`, request.toJson());
+        return await this.put(`/Cliente/${id}`, request.toJson());
     }
 
     async deleteCliente(id) {
-        return await this.delete(`/api/Cliente/${id}`);
+        return await this.delete(`/Cliente/${id}`);
     }
 }
